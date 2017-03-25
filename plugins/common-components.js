02e22283ff79aeda49a2e559f897077919d6bc56
KonstantinVlasov <konstantinnvlasov@gmail.com>
2017-03-25 13:53:42 +0300

    remove old AppButton component

--- plugins/common-components.js
+++ plugins/common-components.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import AppButton from '~components/common/AppButton.vue'
 import DatePicker from '~components/common/DatePicker.vue'
 import Pagination from '~components/common/Pagination.vue'
 import Slider from '~components/common/Slider.vue'
 import SliderItem from '~components/common/SliderItem.vue'
 
-Vue.component('app-button', AppButton)
 Vue.component('date-picker', DatePicker)
 Vue.component('pagination', Pagination)
 Vue.component('slider', Slider)
